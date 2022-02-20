8277d2a504a00a737dc3b6d87f5c35fa39a5e49e
bapnam <ndnam1411@gmail.com>
2022-02-20 22:34:34 +0700

    update

---
+++ ThucHanh/QL_SuDungNuoc111/app/src/main/java/thud/ql_sudungnuoc/giaodien/ThongBao.java
@@ -0,0 +1,16 @@
+package thud.ql_sudungnuoc.giaodien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import thud.ql_sudungnuoc.R;
+
+public class ThongBao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_bao);
+    }
+}
